18c6479ce314f8dbb153d13393d2f2237fdabcd4
Navaneeth Rao <decodestudios.help@gmail.com>
2024-04-29 20:10:52 +0530

    feat: initial server creation with hetzner

---
+++ infrastructure/hcloud.tf
@@ -0,0 +1,25 @@
+variable "hcloud_token" {
+  sensitive = true
+}
+
+# Configure the Hetzner Cloud Provider
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
+# Create a server
+resource "hcloud_server" "foss" {
+  name        = "foss"
+  image       = "debian-12"
+  server_type = "cx11"
+  datacenter = "nbg1-dc3"
+  ssh_keys = [ "16754374" ]
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+}
+
+# List SSH keys
+# data "hcloud_ssh_keys" "all_keys" {
+# }
