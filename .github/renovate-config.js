02156b8891d72fa41abf48af8ae0596220956a0e
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-08-09 19:26:09 +0530

    feat: renovatebot

---
+++ .github/renovate-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  username: 'renovate-release',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  onboarding: false,
+  platform: 'github',
+  forkProcessing: 'enabled',
+  dryRun: 'full',
+  // repositories: ['renovate-tests/cocoapods1', 'renovate-tests/gomod1'],
+  packageRules: [
+    {
+      description: 'lockFileMaintenance',
+      matchUpdateTypes: [
+        'pin',
+        'digest',
+        'patch',
+        'minor',
+        'major',
+        'lockFileMaintenance',
+      ],
+      dependencyDashboardApproval: false,
+      minimumReleaseAge: 0,
+    },
+  ],
+};
